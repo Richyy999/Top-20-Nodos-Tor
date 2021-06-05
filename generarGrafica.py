6ddb9363c8afaf947b6e463d00a053d327fcce7d
Ricardo Bordería Pi <rychyy999@gmail.com>
2021-06-05 20:40:09 +0200

    Añadido opción IP específica

--- generarGrafica.py
+++ generarGrafica.py
@@ -1,26 +1,44 @@
 
 from matplotlib import pyplot as plt
+
 from datetime import datetime as date
+
 from collections import OrderedDict
+
+import numpy as np
+
 from os import listdir
 from os import path
 from os.path import isfile
 
+import os
+
 from guardarTop20 import Nodo
 from guardarTop20 import RESULTS_FOLDR
+from guardarTop20 import Log
 
 def leerTodo():
 	diccionarioNodos = dict()
 	for file in listdir(RESULTS_FOLDR):
-		dia = date.strptime(path.splitext(file)[0], '%Y-%m-%d-%H_%M_%S')
+		try:
+			dia = date.strptime(path.splitext(file)[0], '%Y-%m-%d-%H_%M_%S')
 		
-		listaNodos = []
-		archivo = open(RESULTS_FOLDR + file)
-		for linea in archivo:
-			datos = linea.split(';')
-			listaNodos.append(Nodo(datos[0], datos[1], int(datos[2])))
+			listaNodos = []
+			archivo = None
+			archivo = open(RESULTS_FOLDR + file)
+			for linea in archivo:
+				datos = linea.split(';')
+				listaNodos.append(Nodo(datos[0], datos[1], int(datos[2])))
 
-		diccionarioNodos.update({dia : listaNodos})
+			diccionarioNodos.update({dia : listaNodos})
+		except ValueError as v:
+			log = Log()
+			log.error(v)
+			log.close()
+		except PermissionError as p:
+			log = Log()
+			log.error(p)
+			log.close()
 
 	nodosOrdenados = OrderedDict(sorted(diccionarioNodos.items()))
 
@@ -102,8 +120,8 @@ def generarGraficaGeneral():
 			plt.plot(dias, v, marker='.')
 		indice = indice + 1
 
-	plt.ylabel('Ancho de banda')
-	plt.xlabel('Días')
+	plt.ylabel('Ancho de banda (bps)')
+	plt.xlabel('Día')
 	plt.title('Top 20 IPs con más ancho de banda')
 	plt.legend()
 	plt.show()
@@ -131,17 +149,20 @@ def generarTop5():
 		plt.plot(dias, v, marker='.', label=listaNombres[indice])
 		indice = indice + 1
 
-	plt.ylabel('Ancho de banda')
-	plt.xlabel('Días')
+	plt.ylabel('Ancho de banda (bps)')
+	plt.xlabel('Día')
 	plt.title('Top 5 nodos con más ancho de banda')
 	plt.legend()
 	plt.show()
 
 def generarGraficaUnDia():
-	diaStr = input('Indica el día con el formato día-mes-año:')
+	diaStr = input('Indica el día con el formato día-mes-año: ')
 	dia = date.strptime(diaStr, '%d-%m-%Y')
 	diccionarioNodos = leerTodo()
 	nodos = getNodosPorDia(diccionarioNodos, dia)
+	if len(nodos) == 0:
+		print('No hay datos de el día indicado')
+		return
 
 	diccionarioIPs = crearDiccionarioIPs(nodos)
 
@@ -157,22 +178,53 @@ def generarGraficaUnDia():
 			plt.plot(horas, v, marker='.')
 		indice = indice + 1
 
-	plt.ylabel('Ancho de banda')
-	plt.xlabel('Horas')
+	plt.ylabel('Ancho de banda (bps)')
+	plt.xlabel('Hora')
 	plt.title('Ancho de banda del día ' + dia.strftime('%d/%m/%Y'))
 	plt.legend()
 	plt.show()
 
 
+def generarGraficaUnaIP():
+	ip = input('Introduce la IP: ')
+	diccionarioNodos = leerTodo()
+	diccionarioIPs = crearDiccionarioIPs(diccionarioNodos)
+
+	ipSeleccionada = dict()
+	for k, v in diccionarioIPs.items():
+		if k == ip:
+			ipSeleccionada.update({k : v})
+
+	dias = getDias(list(diccionarioNodos.keys()))
+
+	ejeY = np.arange(len(dias))
+
+	try:
+		plt.xticks(ejeY, dias)
+		plt.bar(ejeY, list(ipSeleccionada.values())[0])
+		plt.ylabel('Ancho de banda (bps)')
+		plt.xlabel('Día')
+		plt.title('Ancho de banda de la IP: ' + list(ipSeleccionada.keys())[0])
+
+		plt.show()
+	except IndexError as e:
+		log = Log()
+		log.error(e)
+		log.close()
+		print('La IP seleccionada no existe')
+
+
+#os.system('clear')
 #generarGraficaGeneral()
 #generarTop5()
 #generarGraficaUnDia()
+#generarGraficaUnaIP()
 
 seguir = True
 while seguir:
 	print('Elige una opción:\n1.- Gráfica general\n2.- Top 5 nodos\n3.- Gráfica de un día concreto\n4.- Salir')
 	eleccion = int(input(''))
-
+	os.system('clear')
 	if eleccion == 1:
 		generarGraficaGeneral()
 	elif eleccion == 2:
